0d3ae83cd730d2d2dad4ffa93726a30f451cf400
zhhony <zhhony@126.com>
2022-11-27 14:38:40 +0800

    格式化代码

--- main.py
+++ main.py
@@ -10,10 +10,11 @@ import random
 
 
 def decodeJson(iput: str) -> list:
-    patternStart = re.compile('^datatable\d+\((?=\{)')  # 寻找开头的 datatable123456 字样
+    patternStart = re.compile(
+        '^datatable\d+\((?=\{)')  # 寻找开头的 datatable123456 字样
     patternEnd = re.compile('\);$')  # 寻找结尾的 )" 字样
-    iput = re.sub(patternStart,'',iput)
-    iput = re.sub(patternEnd,'',iput)
+    iput = re.sub(patternStart, '', iput)
+    iput = re.sub(patternEnd, '', iput)
     iput2Dict = json.loads(iput)
     return iput2Dict['result']['data']
 
@@ -35,7 +36,7 @@ headers = {"Accept": "*/*",
            "Sec-Fetch-Site": "same-site",
            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"}
 
-payload = {"callback": "datatable6013649",
+payload = {"callback": "datatable6013649",  # 源码:rnd = 'datatable' + Math.floor(Math.random() * 10000000 + 1);
            "columns": "REPORT_DATE,TIME,NATIONAL_SAME,NATIONAL_BASE,NATIONAL_SEQUENTIAL,NATIONAL_ACCUMULATE,CITY_SAME,CITY_BASE,CITY_SEQUENTIAL,CITY_ACCUMULATE,RURAL_SAME,RURAL_BASE,RURAL_SEQUENTIAL,RURAL_ACCUMULATE",
            "pageNumber": 0,
            "pageSize": 20,
@@ -53,13 +54,14 @@ url = r'https://datacenter-web.eastmoney.com/api/data/v1/get?'
 
 result = []
 for i in range(1, 10):
-    payload['pageNumber'], payload['p'], payload['pageNo'], payload['pageNum'], payload['_'],payload['callback'] = i, i, i, i, int(
-        time.time()),'datatable' + str(random.randint(1000000,9999999))
+    payload['pageNumber'], payload['p'], payload['pageNo'], payload['pageNum'], payload['_'], payload['callback'] = i, i, i, i, int(
+        time.time()), 'datatable' + str(random.randint(1000000, 9999999))
     data = parse.urlencode(payload)
     userRequest = request.Request(
         url=url + data, headers=headers, method='GET')
 
     response = request.urlopen(userRequest)
-    responseDataList = decodeJson(gzip.decompress(response.read()).decode('utf-8'))
+    responseDataList = decodeJson(
+        gzip.decompress(response.read()).decode('utf-8'))
     for j in responseDataList:
         result.append(j)
